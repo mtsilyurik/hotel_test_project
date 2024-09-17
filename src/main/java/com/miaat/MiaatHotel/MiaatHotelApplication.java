89e7a082c2430b57c9f4b007cc83c957729d16ea
Mikhail Tsilyurik <mtsilyurik@yandex.ru>
2024-09-17 12:01:07 +0300

    Init

---
+++ src/main/java/com/miaat/MiaatHotel/MiaatHotelApplication.java
@@ -0,0 +1,13 @@
+package com.miaat.MiaatHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaatHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiaatHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miaat/MiaatHotel/MiaatHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaat.MiaatHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaatHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
