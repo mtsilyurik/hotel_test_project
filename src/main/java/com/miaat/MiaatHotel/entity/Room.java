3c7da00813577ff20acb35ea3ff2bfc8f0f1ef4d
Mikhail Tsilyurik <mtsilyurik@yandex.ru>
2024-09-25 11:38:52 +0300

    JWT service added

--- src/main/java/com/miaat/MiaatHotel/entity/Room.java
+++ src/main/java/com/miaat/MiaatHotel/entity/Room.java
@@ -24,8 +24,7 @@ public class Room {
 
     private String roomDescription;
 
-//    @OneToMany
-    @ElementCollection
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Booking> bookings = new ArrayList<>();
 
     @Override
